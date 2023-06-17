3c1493257e5d86899549957c5c5e28f6effcd79e
NafisMahmudAyon <nafismahmudayon@gmail.com>
2023-06-17 20:23:52 +0600

    fb messanger add

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,5 +1,6 @@
 // import Home from "./Home"
 import HomeMobile from "./HomeMobile"
+import MessengerCustomerChat from 'react-messenger-customer-chat';
 
 const HomePage = () => {
   return (
@@ -7,6 +8,12 @@ const HomePage = () => {
         {/* <div className="sm:hidden md:hidden lg:block xl:block 2xl:block"><Home /> </div> */}
         {/* <div className="sm:block md:block lg:hidden xl:hidden 2xl:hidden "><HomeMobile /> </div> */}
         <div className="sm:block md:block "><HomeMobile /> </div>
+        <MessengerCustomerChat
+    pageId="118142634620388"
+    appId="1325514468346747"
+    htmlRef="<REF_STRING>"
+  />
+  118142634620388
     </div>
   )
 }
